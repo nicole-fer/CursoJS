2370bf836065668929312cc56b5b89cc55db2b83
nicole-fer <nicolefernandes@alu.ufc.br>
2022-04-25 15:03:04 -0300

    Somar valores retornando funções a partir de outra função

--- Funções/desafio_1.js
+++ Funções/desafio_1.js
@@ -1,1 +1,12 @@
-//
+// somar(3)(4)(5)
+
+function somar(n1){
+    return function(n2){
+        return function(n3){
+            return n1+n2+n3
+        }
+    }
+}
+
+const somaN = somar(1)(2)(3)
+console.log(somaN)
