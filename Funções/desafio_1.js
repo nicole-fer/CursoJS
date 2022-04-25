a1fdc98008c9e1cd3d73fcce1e0ec6931699d348
nicole-fer <nicolefernandes@alu.ufc.br>
2022-04-25 15:26:24 -0300

    A função de retorno indica que opeação sera usada para os dois primeiros valores de retorno

--- Funções/desafio_1.js
+++ Funções/desafio_1.js
@@ -10,3 +10,43 @@ function somar(n1){
 
 const somaN = somar(1)(2)(3)
 console.log(somaN)
+
+//calcular (3)(7)(fn)
+console.log('------------------------------------PARTE II ----------------------------------')
+
+function calcula(v1){
+    return function(v2){
+        return function (fn){
+            return fn(v1,v2)
+        }
+    }
+}
+function subtrair(a,b){
+    return a-b
+}
+
+function somar2(a,b){
+    return a+b
+}
+
+function multiplicar(a,b){
+    return a*b
+}
+
+function dividir(a,b){
+    return a/b
+}
+
+console.log('Valores -> 10 e 5')
+
+const subtraiV = calcula(10)(5)(subtrair)
+console.log( 'SUBTRAÇÃO -> ',subtraiV)
+
+const somaV = calcula(10)(5)(somar2)
+console.log('SOMA -> ',somaV)
+
+const multiplicaV = calcula(10)(5)(multiplicar)
+console.log('MULTIPLICAÇÃO -> ',multiplicaV)
+
+const divideV = calcula(10)(5)(dividir)
+console.log('DIVISÃO -> ',divideV,)
