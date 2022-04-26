e474ccde0a2eacc0beabdf7eefb53b4d6edf4913
nicole-fer <nicolefernandes@alu.ufc.br>
2022-04-26 13:47:49 -0300

    ler outro arquivo com arrow function

--- Funções/funcaoCallBack_2.js
+++ Funções/funcaoCallBack_2.js
@@ -3,5 +3,12 @@ const path = require('path')
 
 const caminho = path.join(__dirname, 'dados.txt')
 
-fs.readFile(caminho,{},)
+function exibirConteudo(err, conteudo){
+    console.log(conteudo.toString())
+}
+
+fs.readFile(caminho,{},exibirConteudo)
+
+//função com arrow function 
+fs.readFile(caminho,(_, conteudo) => console.log(conteudo.toString()))
 
