0a983a7d23f063faf2621b52062a37d4984a8df6
nicole-fer <nicolefernandes@alu.ufc.br>
2022-04-26 16:26:34 -0300

    Media dos valores (preco*qtde) que sejam fragil:true

---
+++ Funções/desafio_2.js
@@ -0,0 +1,33 @@
+// ----------------------------- DESAFIO 02 ---------------------------------------
+
+const carrinho = [
+    {nome: 'Caneta', qtde: 10, preco: 7.99, fragil: true},
+    {nome: 'Impressora', qtde: 1, preco: 649.50, fragil: true},
+    {nome: 'Caderno', qtde: 4, preco: 27.10, fragil: false},
+    {nome: 'Lapis', qtde: 3, preco: 5.82, fragil: false},
+    {nome: 'Tesoura', qtde: 1, preco: 19.20, fragil: true},
+]
+
+// apenas elementos que tenham fagil true
+console.log("---------- Pegar os elementos que tenham fragil: true ----------")
+const getFragil = a => a.fragil === true
+console.log(carrinho.filter(getFragil))
+
+// pegar quantidade e preco -> qtde*preco = total
+console.log("---------- Pegar Quantidade * Preço ----------")
+const valor = (a) => (a.qtde)*(a.preco)
+
+const total =  carrinho.filter(getFragil).map(valor)
+
+console.log(total)
+
+// gerar a media dos totais
+console.log("---------- Pegar a média dos totais ----------")
+tamanho = total.length
+
+const soma = (a,b) => { 
+    return  a+b
+}
+
+const somaTotais=  carrinho.filter(getFragil).map(valor).reduce(soma)
+console.log('MEDIA ->',somaTotais/tamanho)
