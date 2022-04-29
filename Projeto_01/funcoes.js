2378d5de492161fe3fba6bc105b5becb8ed042cb
nicole-fer <nicolefernandes@alu.ufc.br>
2022-04-29 16:57:38 -0300

    atualização #Projeto_01

---
+++ Projeto_01/funcoes.js
@@ -0,0 +1,20 @@
+const fs = require ('fs')
+const path = require('path')
+
+function lerDiretorio(caminho){
+    return new Promise((resolve, reject) => {
+        try {
+            let arquivos = fs.readdirSync(caminho)
+            arquivos.map(arquivo => path.join(caminho,arquivo))
+            resolve(arquivos)
+        } catch (e){
+            reject(e)
+        }
+    })
+    
+
+}
+
+module.exports = {
+    lerDiretorio
+}
