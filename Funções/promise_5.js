7fd671bb6a8ce275461c13203ab0da58fce9fb22
nicole-fer <nicolefernandes@alu.ufc.br>
2022-04-29 14:13:43 -0300

    Alterações Função Promise #05

--- Funções/promise_5.js
+++ Funções/promise_5.js
@@ -1,14 +1,23 @@
 function funcionarOuNao (valor, chanceErro){
     return new Promise((resolve, reject) =>{
-        if(Math.random()< chanceErro){
-            reject('Ocorreu um erro')
-        } else{
-            resolve(valor)
+        try{
+            if(Math.random()< chanceErro){
+                reject('Ocorreu um erro')
+            } else{
+                resolve(valor)
+            }
+        } catch(e){
+            reject(e)
         }
     })
 }
 
 funcionarOuNao('testando...',0.5)
     .then(v => console.log(`Valor: ${v}`))
-    .catch(er => console.log(`Erro: ${er}`))
+    .then(
+        v => console.log(v),
+        err => console.log(`Erro Esp.: ${err}`)
+    )
+    .then(()=> console.log('Quase fim'))
+    .catch(er => console.log(`Erro: ${er}`))    //geralmente chatch é no final
     .then(() => console.log('Fim!'))
