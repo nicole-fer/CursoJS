eb03c14cfbd2f5da036f89c0e3a2dab6df1ff2cd
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-02 15:38:35 -0300

    Atualizando

--- Projeto_01/index.js
+++ Projeto_01/index.js
@@ -4,7 +4,8 @@ const fn = require('./funcoes')
 const caminho = path.join(__dirname,'..','dados','legendas','legendas')
 
 fn.lerDiretorio(caminho)
-    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-    .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
-    .then(conteudos => conteudos.join(''))
+    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt')) //filtra os arquivos srt
+    .then(arquivosSRT => fn.lerArquivos(arquivosSRT)) //ler todo os conteudo
+    .then(conteudos => conteudos.join('\n'))      // o array passa a ser uma grande string com todos os arquivos
+    .then(TodoConteudo => TodoConteudo.split('\n')) //separa em varias linhas
     .then(console.log)
