08a065c4f6d1e4cb110daa63b91c672cfa1fbb5c
nicole-fer <nicolefernandes@alu.ufc.br>
2022-05-02 17:05:13 -0300

    Separando palavras

--- Projeto_01/index.js
+++ Projeto_01/index.js
@@ -6,14 +6,20 @@ const simbolos = [
     '.', '?', '-', '"', '♪' , '_', '<i>', '</i>', '\r', '[', ']','(', ')'
 ]
 
+const mesclarConteudos = array => array.join('\n')
+const separarPorLinhas = todoConteudo => todoConteudo.split('\n')
+const separarPorPalavras = todoConteudo => todoConteudo.split(' ')
+
 fn.lerDiretorio(caminho)
     .then(fn.elementosTerminadosCom('.srt')) //filtra os arquivos srt
     .then(fn.lerArquivos) //ler todo os conteudo
-    .then(conteudos => conteudos.join('\n'))      // o array passa a ser uma grande string com todos os arquivos
-    .then(TodoConteudo => TodoConteudo.split('\n')) //separa em varias linhas
+    .then(mesclarConteudos)      // o array passa a ser uma grande string com todos os arquivos
+    .then(separarPorLinhas) //separa em varias linhas
     .then(fn.removerSeVazio)  //remove linhas vazias e algumas linhas consideradas vazias como /r
     .then(fn.removerSeIncluir('-->'))
     .then(fn.removerSeIncluir('</font>'))
     .then(fn.removerSeApenasNumero)
     .then(fn.removerSimbolos(simbolos))
+    .then(mesclarConteudos)
+    .then(separarPorPalavras)
     .then(console.log)
